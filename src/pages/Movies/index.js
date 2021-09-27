82430a09ba1447b6ef54e187a282480fc9b9d8a0
Alanna <alannapaiva1@hotmail.com>
2021-09-27 16:28:18 -0300

    Create Pages and Routes

---
+++ src/pages/Movies/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function Movies(){
+    return(
+        <View>
+            <Text>TELA MOVIE</Text>
+        </View>
+    )
+}
+
+export default Movies; 
