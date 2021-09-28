d04edf2ef2375c5825115c3b5cd461fc717eea24
Alanna <alannapaiva1@hotmail.com>
2021-09-27 23:12:25 -0300

    Create the Input and Button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import { Container} from './styles';
+import { Container, SearchContainer, Input, SearchButton } from './styles';
+import { Feather } from '@expo/vector-icons';
+
 import Header from '../../components/Header';
 
 function Home(){
     return(
         <Container>
             <Header title = "React Prime" />
-            <Text>TELA HOME</Text>
+            <SearchContainer>
+                <Input 
+                    placehodler = "Ex: Vingadores"
+                />
+                <SearchButton>
+                    <Feather name="search" size={30} color="#fff" />
+                </SearchButton>
+            </SearchContainer>
         </Container>
     )
 }
