70057633b3bb613341928f6e301443b28c0f24b6
Alanna <alannapaiva1@hotmail.com>
2021-10-03 16:06:18 -0300

    Created BannerButton component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView } from 'react-native';
 
-import { Container, SearchContainer, Input, SearchButton } from './styles';
+import { 
+    Container, 
+    SearchContainer, 
+    Input, 
+    SearchButton, 
+    Title, 
+    BannerButton, 
+    Banner 
+} from './styles';
 
 import { Feather } from '@expo/vector-icons';
 import Header from '../../components/Header';
-
 
 
 function Home(){
@@ -20,6 +27,17 @@ function Home(){
                     <Feather name="search" size={30} color="#fff" />
                 </SearchButton>
             </SearchContainer>
+
+            <ScrollView>
+                <Title> Em Cartaz </Title>
+
+                <BannerButton activeOpacity={0.8} onPress={ () => alert('TESTE')}>  
+                    <Banner
+                    resizeMethod="resize"
+                    source={ {uri:'https://poltronanerd.com.br/wp-content/uploads/2018/11/poltrona-Marvel-Studios-The-First-10-Years-Banner.jpg'} }
+                    />
+                </BannerButton>
+            </ScrollView>
         </Container>
     )
 }
