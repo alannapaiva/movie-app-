6472a06912e10c61884fe31db6cc564d6ca34596
Alanna <alannapaiva1@hotmail.com>
2021-10-05 11:46:08 -0300

    Change the random link for the API route

--- src/components/SliderItem/index.js
+++ src/components/SliderItem/index.js
@@ -11,17 +11,17 @@ import {
 import {Ionicons} from '@expo/vector-icons';
 
 
-function SliderItem(){
+function SliderItem( {data}){
   return(
     <Container activeOpacity={0.7}>
       <BannerItem 
-        source={{uri: 'https://i.pinimg.com/236x/bf/2e/91/bf2e91ff2203add4e60dd8b913135698--marvel-dc-comics-poster-marvel.jpg'}}
+        source={ {uri: `https://image.tmdb.org/t/p/original/${data.poster_path}` }}
       />
 
-      <Title numberOfLines={1}> Doutor Estranho</Title>
+      <Title numberOfLines={1}> {data.title}</Title>
       <RateContainer>
         <Ionicons name="md-star" size={12} color= "#E7A74e" /> 
-        <Rate>9/10</Rate>
+        <Rate>{data.vote_average}/10</Rate>
       </RateContainer>
     </Container>
   )
