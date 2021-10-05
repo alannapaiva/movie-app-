059fa3fdd8a2d03af29416ada05d6de5ee0fb45d
Alanna <alannapaiva1@hotmail.com>
2021-10-05 11:30:22 -0300

    Changed some words

--- src/utils/movie.js
+++ src/utils/movie.js
@@ -1,5 +1,5 @@
 // Gerar uma lista de filmes com o tamanho que eu desejar
-export function getLISTmovies(size, movies){
+export function getListMovies(size, movies){
     let popularMovies = [];
 
     for(let i = 0, l = size; i< l; i++){
